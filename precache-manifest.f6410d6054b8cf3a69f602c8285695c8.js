2980328d091d68e06415192ad617121430094293
Shunmugam V <v.shunmugam@thoughtworks.com>
2020-10-03 22:52:37 +0530

    Updates

---
+++ precache-manifest.f6410d6054b8cf3a69f602c8285695c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c768f3d7ad53d4ebfdcc0e5fbf05b0",
+    "url": "/apiman/index.html"
+  },
+  {
+    "revision": "dced966538f53fd0f435",
+    "url": "/apiman/static/js/2.54967c05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/apiman/static/js/2.54967c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17768304581142903957",
+    "url": "/apiman/static/js/main.fd99498d.chunk.js"
+  },
+  {
+    "revision": "813a57671696c88bbb32",
+    "url": "/apiman/static/js/runtime-main.d560ec54.js"
+  },
+  {
+    "revision": "1b07858a40080a96f8c28d4d08e595a1",
+    "url": "/apiman/static/media/apiman.1b07858a.svg"
+  }
+]);
